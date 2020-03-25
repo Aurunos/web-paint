4c4bb32dcc334271fa6326e9e5d8630852dccbc7
Aurunos <emberwolfdev@gmail.com>
2020-03-25 02:25:12 -0400

    Added toolbar that fades out with one initial button.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var myCanvas = document.getElementById("main-canvas");
+myCanvas.addEventListener("click", function (event) {
+    alert("I have been clicked at " + event.x + ", " + event.y + "!");
+});
+var mainToolbar = document.getElementById("toolbar");
+mainToolbar.addEventListener("mouseleave", function () {
+    this.style.transition = '0.8s';
+    this.style.opacity = "0";
+});
+mainToolbar.addEventListener("mouseenter", function () {
+    this.style.transition = '0.8s';
+    this.style.opacity = "1";
+});
--- js/main.ts
+++ js/main.ts
@@ -1,1 +1,16 @@
+let myCanvas = document.getElementById("main-canvas");
+myCanvas.addEventListener("click", function (event) {
+
+  alert(`I have been clicked at ${event.x}, ${event.y}!`);
+});
+
+let mainToolbar = document.getElementById("toolbar");
+mainToolbar.addEventListener("mouseleave", function () {
+  this.style.transition = '0.8s';
+  this.style.opacity = "0";
+});
+mainToolbar.addEventListener("mouseenter", function () {
+  this.style.transition = '0.8s';
+  this.style.opacity = "1";
+});
 
